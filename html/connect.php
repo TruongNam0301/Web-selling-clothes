6f31589e104da285e8297eb69e358e3bd1c37d56
Manh Hieu <phanhieu205@gmail.com>
2020-07-02 11:41:18 +0700

    more css for product page

--- html/connect.php
+++ html/connect.php
@@ -29,16 +29,22 @@
   };
 
   function OutputDataGridView ($id,$name,$price,$image){
+    $p=number_format($price,0,",",".");
     $str = "
       <div class='col-lg-4'>
           <div class='product sanpham'>
           <a href='product.php?id=$id'>
               <div class='image-item'>
-                  <img src='../image/image_product/$image' class='item' >
+                  <img src='../image/image_product/$image' class='item' style='z-index:-1' >
+                  <div class='view-detail' >
+                      <b>View Detail</b>
+                </div>
               </div>
-              <div class='item-detail'>
-                  <span class='item-title'>$name</span>
-                  <span class='item-price'>$price</span>
+              
+              <div class='item-detail' align='center'>
+                  
+                  <p class='item-title' style='font-size: large;color:#575652;transform: translate(0px,10px);'>$name</p>
+                  <p class='item-price' style='color:#575652;'><b>$p <sup><u>đ</u></sup></b></p>
               </div>
               </a>
           </div>
