8d5e39d94b1a20fe4d1cbf3cc5b88b97bb558312
Manh Hieu <phanhieu205@gmail.com>
2020-07-13 12:13:02 +0700

    add data provider && database

--- models/TypeClothesMdl.php
+++ models/TypeClothesMdl.php
@@ -1,13 +1,19 @@
 <?php
+    include_once("DataProvider.php");
     class TypeClothesMdl{
+        private $db;
+
+        function __construct(){
+            $this->db = new DataProvider(); 
+        }
         public function getTypeClothes(){
-            $conn = mysqli_connect("localhost","root","","sellclothes");
+          
             $sql = "SELECT * FROM typeclothes";
-            $result = mysqli_query($conn,$sql);
+            $types = $this->db->FetchAll($sql);
             $typeClothes = array();
-            if(mysqli_num_rows($result)>0){
-                while($type = mysqli_fetch_assoc($result)){
-                    $typeClothes[] = $type;
+            if($this->db->NumRows($sql)){
+               foreach($types as $types){
+                    $typeClothes[] = $types;
                 }
                 return $typeClothes;
             }
