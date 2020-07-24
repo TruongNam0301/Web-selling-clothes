35521c511b21c36cac15ddd339c3970a3a764253
Manh Hieu <phanhieu205@gmail.com>
2020-07-24 13:53:10 +0700

    css admin page and fix account manager

--- admin-test/TYPES.php
+++ admin-test/TYPES.php
@@ -29,7 +29,7 @@ if(isset($_POST['action'])){
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Static Navigation - SB Admin</title>
+        <title>HNH SHOP  Admin</title>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
@@ -38,7 +38,7 @@ if(isset($_POST['action'])){
     </head>
     <body>
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.php">Start Bootstrap</a>
+            <a class="navbar-brand" href="index.php">HNH Shop Admin Page</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
@@ -69,10 +69,10 @@ if(isset($_POST['action'])){
             <div id="layoutSidenav_content">
                 <main>
                     <div class="container-fluid">
-                        <h1 class="mt-4">Static Navigation</h1>
+                        <h1 class="mt-4">TYPES MANAGER</h1>
                         <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Static Navigation</li>
+                            <li class="breadcrumb-item"><a href="index.php">DASHBOARD</a></li>
+                            <li class="breadcrumb-item active">TYPES</li>
                         </ol>
                         
 
@@ -130,7 +130,7 @@ if(isset($_POST['action'])){
 <div class="card mb-4">
     <div class="card-header" >
         <i class="fas fa-table mr-1"></i>
-        Product List
+        TYPE LIST
         <div style="float:right">
             <button class='btn-add btn btn-success'  data-toggle='modal' data-target='#AddModal'><i class="fa fa-plus" aria-hidden="true"></i>Add New Type </button>
         </div>
--- views/admin/Bills.php
+++ views/admin/Bills.php
@@ -2,8 +2,8 @@
     foreach($array as $item){
         @$p=number_format($item['total'],0,",",".");
         $check=$item['tinhtrang'];
-        if($check==0)$check = 'chua xac nhan';
-        else $check= 'da xac nhan';
+        if($check==0)$check = 'UNPAID';
+        else $check= 'PAID';
         echo "<tr>";
         echo "<td ><img src='../image/image-user/$item[image]' class='image' style='width:100px; height:50px' ></td>";
             echo "<td class='name'>$item[name]</td>";
@@ -12,9 +12,9 @@
             echo "<td class='date'>$item[date]</td>";
             echo "<td class='tinhtrang' data-value='$item[tinhtrang]'>$check</td>";
             echo "<td class='total'>$p</td>";
-            echo "<td><button class='btn-edit btn btn-primary' data-mahd=$item[MaHD] data-toggle='modal' data-target='#exampleModal'>EDIT</button>";
-            echo "<a href='chitiet_hoadon.php?MaHD=$item[MaHD]'><button class='btn-detail btn btn-success' style='margin-left:10px' >detail bills</button></a> ";
-            echo "<a href='?tt=$_GET[tt]&&action=delete&&MaHD=$item[MaHD]'><button class='btn-delete btn btn-danger' style='margin-left:10px' >delete</button></td> ";
+            echo "<td><button class='btn-edit btn btn-primary' data-mahd=$item[MaHD] data-toggle='modal' data-target='#exampleModal' style='margin:2px 0px 0px 20px; width:150px'>EDIT</button>";
+            echo "<a href='chitiet_hoadon.php?MaHD=$item[MaHD]&&tt=$_GET[tt]'><button class='btn-detail btn btn-success' style='width:150px;margin:2px 0px 0px 20px' >VIEW DETAIL</button></a> ";
+            echo "<a href='?tt=$_GET[tt]&&action=delete&&MaHD=$item[MaHD]'><button class='btn-delete btn btn-danger' style='width:150px;margin:2px 0px 0px 20px' >DELETE</button></td> ";
         echo "</tr>";
     }          
 
